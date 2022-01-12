e151a1f5633aa4a1146f1c3795c94ed915653667
ahmed-saim-hamid <ahmedsamihamid1@gmail.com>
2022-01-12 16:56:52 +0200

    try to fix it

--- src/components/body/exploreD.js
+++ src/components/body/exploreD.js
@@ -48,17 +48,17 @@ render() {
 
                     <div className="flex">
                         
-                        <svg xmlns="http://www.w3.org/2000/svg" class="flex mx-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="flex mx-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
 </svg>
                         
                         
-                        <svg xmlns="http://www.w3.org/2000/svg" class="flex mx-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="flex mx-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
 </svg>
                         
                         
-                        <svg xmlns="http://www.w3.org/2000/svg" class="flex mx-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="flex mx-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
 </svg>
                         
